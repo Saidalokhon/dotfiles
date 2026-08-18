afe65c3c34c3e5b0225edcaef67ac6f763d607c4
Sidalokhon Saydakhmedov <saydakhmedovv@gmail.com>
2026-08-19 02:29:53 +0500

    Add options to lazyvim

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+vim.opt.shiftwidth = 4         -- Size of an indent
+vim.opt.tabstop = 4            -- Number of spaces tabs count for
+vim.opt.softtabstop = 4        -- Number of spaces that a Tab counts for while performing editing operations
+vim.opt.expandtab = true       -- Use spaces instead of tabs
+
+vim.opt.relativenumber = false -- Turn off relative numbers
